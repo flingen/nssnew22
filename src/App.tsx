8fc5f79e24372afa0954be74be905dfeecc367af
Flin <166637033+flingen@users.noreply.github.com>
2026-04-16 14:43:24 +0100

    Add files via upload

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { useEffect } from 'react';
+import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom';
 import { Header } from './components/layout/Header';
 import { Footer } from './components/layout/Footer';
 import { Hero } from './components/sections/Hero';
@@ -18,6 +19,23 @@ import { AdminPage } from './pages/AdminPage';
 import { TravelPage } from './pages/TravelPage';
 import { SpeakersPage } from './pages/SpeakersPage';
 import { AgendaPage } from './pages/AgendaPage';
+
+// Sends a Google Analytics pageview every time the route changes.
+function GoogleAnalyticsTracker() {
+  const location = useLocation();
+
+  useEffect(() => {
+    // @ts-expect-error gtag is injected by the script in index.html
+    if (typeof window !== 'undefined' && typeof window.gtag === 'function') {
+      // @ts-expect-error gtag is injected by the script in index.html
+      window.gtag('config', 'G-B8L10Q532E', {
+        page_path: location.pathname + location.search,
+      });
+    }
+  }, [location]);
+
+  return null;
+}
 
 function HomePage() {
   return (
@@ -46,6 +64,7 @@ function HomePage() {
 function App() {
   return (
     <BrowserRouter>
+      <GoogleAnalyticsTracker />
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/travel" element={<TravelPage />} />
